d683261fb083e7b2155501d365affadc4da0cf5f
Ramrachai <ramrachaim@gmail.com>
2022-03-18 21:52:00 +0600

    moviezone app code writing complete. Now final copy commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "./counter.css";
+import SearchIcon from "./search.svg";
+import MovieCard  from "./MovieCard";
+
+// OMDB api key: c14c64ec
+// const OMDB_API_KEY = "c14c64ec";
+const Api_url = `http://www.omdbapi.com/?apikey=c14c64ec`;
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [searchTerm, setsearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
 
-export default App
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${Api_url}&s=${title}`);
+    const data = await response.json();
+    setMovies(data.Search);
+    console.log(movies);
+  };
+  const enterHandler = (e) => {
+    if (e.key == 'Enter') {
+      searchMovies(searchTerm)
+    }
+  }
+  return (
+    <div className="app">
+      <h1 >Movie Zone</h1>
+      <div className="search">
+        <input
+          type="text"
+          value={searchTerm}
+          placeholder="Enter a Movie name"
+          onKeyUp={enterHandler}
+          onChange={(e) => {
+            setsearchTerm(e.target.value);
+          }}
+        />
+        <img src={SearchIcon} alt="Search Icon" onClick={()=>{searchMovies(searchTerm)}} />
+      </div>
+
+      {
+        movies?.length > 0 ? (
+          <div className="container"> 
+            {
+              movies.map( (movie)=> (<MovieCard movie = {movie} />))
+            }
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found </h2>
+          </div>
+        )
+      }
+    </div>
+  );
+};
+
+export default App;
